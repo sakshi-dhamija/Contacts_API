b1aad7d68592aa77a6ef719dc1e9145c5706db1a
sakshi-dhamija <sakshidhamija15@gmail.com>
2022-12-23 22:34:10 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+
+const { API_PORT } = process.env;
+const port =  API_PORT;
+
+
+// server listening 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
